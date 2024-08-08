6ff7f2eaa986a1b3104dee11e22a4409b2424778
zmiiovskyi <zmiiovskyi.m@gmail.com>
2024-08-08 20:10:49 +0300

    add shots

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -21,12 +22,14 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Розпочати головний цикл гри."""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -46,6 +49,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """Реагувати, коли клавіша не натиснута"""
@@ -56,10 +61,17 @@ class AlienInvasion:
         elif event.key == pygame.K_q:
             sys.exit()
 
+    def _fire_bullet(self):
+        """Створити нову кулю та додати її до групи куль"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Оновити зображення на екрані та перемкнутись на новий екран"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         pygame.display.flip()
 
---
+++ bullet.py
@@ -0,0 +1,31 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """Клас курування кулями, випущеними з корабля"""
+
+    def __init__(self, ai_game):
+        """Створити об'єкт bullet у поточній позиції корабля"""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        # Створити rect кулі у (0, 0) та задати правильну позицію.
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+            self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        # Зберегти позицію кулі як десяткове значення.
+        self.y = float(self.rect.y)
+
+    def update(self):
+        """Посунути кулю нагору екрану"""
+        # Оновити десяткову позицію кулі
+        self.y -= self.settings.bullet_speed
+        # Оновити позицію rect
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Намалювати кулю на екрані"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
