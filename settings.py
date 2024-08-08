fa91c6a3c5402b77bc969460b766abede23b4323
zmiiovskyi <zmiiovskyi.m@gmail.com>
2024-08-08 19:34:59 +0300

    add bullet settings

--- settings.py
+++ settings.py
@@ -10,3 +10,9 @@ class Settings:
 
         # Налаштування корабля
         self.ship_speed = 1.5
+
+        # Налаштування кулі
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
